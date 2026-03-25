9002b2e6d822a72ca2598c67ab4b6a486d9d993b
saishivaa-coder <sk3771@srmist.edu.in>
2026-03-25 08:29:54 +0530

    Create uc7.java

---
+++ uc7.java
@@ -0,0 +1,15 @@
+public class HelloApp {
+    public static void main(String[] args) {
+
+        // If no arguments → default message
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            // Join all names with ", "
+            String names = String.join(", ", args);
+
+            // Print greeting
+            System.out.println("Hello, " + names + "!");
+        }
+    }
+}
